221b332b047087d2d11e7ff9c0ac70bb8dad5eac
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-30 15:15:19 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,5 +1,9 @@
 class CreateShows < ActiveRecord::Migration
   def change
-    
+    create_tablel :shows do |t|
+      t.string :name 
+      t.string :network
+      t.string :day 
+      t.integer :rating 
   end
 end
