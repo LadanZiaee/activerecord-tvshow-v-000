68dbdc74398b9a4ed616ce99bc365e3275f204ea
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-30 15:50:34 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,8 +5,10 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.highest_rating
+    Show.where(rating: 10).first
   end
   
-  
+  def self.lowest_rating
+    show = Show.minimum(:rating)
+    min = Show.where(rating: show)
 end
